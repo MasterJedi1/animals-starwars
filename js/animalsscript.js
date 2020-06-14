70d2ea5af2c43d3bbe2f48b124197d982cc3b13f
MasterJediKnight <60758334+MasterJediKnight@users.noreply.github.com>
2020-06-14 19:20:10 +0200

    Add files via upload

--- js/animalsscript.js
+++ js/animalsscript.js
@@ -3,20 +3,19 @@
   document.getElementById("drop1").addEventListener("mouseleave", mouseLeave1);
   document.getElementById("drop3").addEventListener("onclick", mouseEnter2);
   document.getElementById("drop3").addEventListener("mouseleave", mouseLeave2);
-  document.getElementById("drop4").addEventListener("onclick", mouseEnter3);  
+  document.getElementById("drop4").addEventListener("onclick", mouseEnter3);
   document.getElementById("drop4").addEventListener("mouseleave", mouseLeave3);
-  
+
   let b_m = document.getElementById("acc");
   let dr = document.getElementById("drop0_1");
-  b_m.addEventListener("click", () =>{
-    if(getComputedStyle(dr).display != "none"){
+  b_m.addEventListener("click", () => {
+    if (getComputedStyle(dr).display != "none") {
       mouseLeave0();
     }
-    else
-    {
+    else {
 
       leaveall();
-      document.getElementById("drop0_1").style.display = "block";   
+      document.getElementById("drop0_1").style.display = "block";
       document.getElementById("drop0_1").style.borderRadius = "10px";
       document.getElementById("acc").innerHTML = "Accueil <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
     }
@@ -35,125 +34,53 @@
       document.getElementById("sw").innerHTML = "Star Wars <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
     }
   })
-  // let b_sith = document.getElementById("sith");
-  // let dr_sith = document.getElementById("Celeste");
-  // b_sith.addEventListener("click", () =>{
-  //   if(getComputedStyle(dr_sith).display != "none"){
-  //     leavesw();
-  //   }
-  //   else
-  //   {
-  //     leavesw();
-  //     replace_footer();
-  //     document.getElementById('Celeste').style.display = 'block';
-  //     document.getElementById('federation').style.display = 'none';
-  //     document.getElementById('Republic').style.display = 'none';
-  //     leaveall();
-  //   }
-  // })
- 
-  let b_repbut = document.getElementById("repbut");
-  let dr_repbut = document.getElementById("Republic");
-  b_repbut.addEventListener("click", () =>{
-    if(getComputedStyle(dr_repbut).display != "none"){
-      // dr.style.display = "none";
-      leavesw();
-      // console.log("heck");
+  let b_anim_sky = document.getElementById("anim_sky");
+  let dr_anim_sky = document.getElementById("celeste");
+  b_anim_sky.addEventListener("click", () => {
+    if (getComputedStyle(dr_anim_sky).display != "none") {
+      leaveanimal();
     }
-    else
-    {
-      leavesw();
-      replace_footer();
-
-      document.getElementById('Republic').style.display = 'block';
+    else {
+      leaveanimal();
+      document.getElementById('celeste').style.display = 'block';
+      document.getElementById('terrestre').style.display = 'none';
+      document.getElementById('maritime').style.display = 'none';
       leaveall();
     }
   })
-  function getRepublic()
-  {
-    var statistics = "https://my-json-server.typicode.com/MasterJediKnight/animals-starwars/Page"
-    fetch(statistics)
-    .then(response => response.json())                                    	 
-    .then(function (stats) {
-        statistics_data = stats;
-        var republic_governor = statistics_data[0].StarWarsI[0].Republic[0].li
-        // console.log(republic_governor);
-        var i = 0;
-        for(i = 0; i < 5; i++)
-        {
-          createRepublic(statistics_data[0].StarWarsI[0].Republic[i], statistics_data[0].StarWarsI[0].Republic[i].li);
-        }
-    });
-  }
-  function createRepublic(element, subelement1)
-  {
-    let newli1 = document.createElement("li");
-    newli1.textContent = subelement1;
-    let divmod= document.getElementById("Republic");
-    divmod.appendChild(newli1);
-    toimage_rep(element);
-  }
-  function toimage_rep(obj) {
-     var img = new Image();
-     img.src = obj.image;
-     img.setAttribute("Id", "myImg");
-     img.setAttribute("onclick", "showImg(this)");
-     img.setAttribute("style","width:100%;max-width:300px");
-     img.setAttribute("alt", obj.alt);
-     document.getElementById("Republic").appendChild(img);
-  }
-  let b_csi = document.getElementById("csi");
-  let dr_csi = document.getElementById("federation");
-  b_csi.addEventListener("click", () =>{
-    if(getComputedStyle(dr_csi).display != "none"){
-      // dr.style.display = "none";
-      leavesw();
+  let b_anim_earth = document.getElementById("anim_earth");
+  let dr_anim_earth = document.getElementById("terrestre");
+  b_anim_earth.addEventListener("click", () => {
+    if (getComputedStyle(dr_anim_earth).display != "none") {
+      leaveanimal();
     }
-    else
-    {
-      leavesw();
-      document.getElementById('federation').style.display = 'block';
-      replace_footer();
+    else {
+      leaveanimal();
+      document.getElementById("Footer").style.bottom = "27px";
+      document.getElementById("allrights").style.bottom = "0";
+      document.getElementById('terrestre').style.display = 'block';
+      leaveall();
+    }
+  })
+  let b_anim_sea = document.getElementById("anim_sea");
+  let dr_anim_sea = document.getElementById("maritime");
+  b_anim_sea.addEventListener("click", () => {
+    if (getComputedStyle(dr_anim_sea).display != "none") {
+      leaveanimal();
+      console.log("heck");
+    }
+    else {
+      leaveanimal();
+      document.getElementById("Footer").style.bottom = "27px";
+      document.getElementById("allrights").style.bottom = "0";
+      document.getElementById('maritime').style.display = 'block';
       leaveall();
     }
   })
 
-  function getSeparatist()
-  {
-    var statistics = "https://my-json-server.typicode.com/MasterJediKnight/animals-starwars/Page"
-    fetch(statistics)
-    .then(response => response.json())                                    	 
-    .then(function (stats) {
-        statistics_data = stats;
-        // console.log(republic_governor);
-        let i = 0;
-        for(i = 0; i < 4; i++)
-        {
-          createSeparatist(statistics_data[0].StarWarsI[1].Separatist[i], statistics_data[0].StarWarsI[1].Separatist[i].li);
-        }
-    });
-  }
-  function createSeparatist(element, subelement1)
-  {
-    let newli1 = document.createElement("li");
-    newli1.textContent = subelement1;
-    let divmod= document.getElementById("federation");
-    divmod.appendChild(newli1);
-    toimage_sep(element);
-  }
-  function toimage_sep(obj) {
-     var img = new Image();
-     img.src = obj.image;
-     img.setAttribute("Id", "myImg");
-     img.setAttribute("onclick", "showImg(this)");
-     img.setAttribute("style","width:100%;max-width:300px");
-     img.setAttribute("alt", obj.alt);
-     document.getElementById("federation").appendChild(img);
-  }
   function mouseLeave0() {
     leaveall();
     document.getElementById("acc").innerHTML = "Accueil <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
-    replace_footer();
   }
 
   //Menu drop 2
@@ -176,7 +103,7 @@
   }
   function mouseLeave2() {
     leaveall();
-    document.getElementById("mamif").innerHTML = "Animaux <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+    document.getElementById("mammif").innerHTML = "Animaux <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
     replace_footer();
   }
   function mouseEnter3() {
@@ -204,7 +131,7 @@
   var modal = document.getElementById("myModal");
   var i;
 
-  var imag = document.getElementsByClassName("myImg");
+  var img = document.getElementsByClassName("myImg");
   var modalImg = document.getElementById("img01");
   var captionText = document.getElementById("caption");
   function showImg(ele) {
@@ -234,13 +161,10 @@
     document.getElementById("Footer").style.transition = "all 1s ease-out;";
     document.getElementById("allrights").style.transition = "all 1s ease-out;";
   }
-  function leavesw()
-  { 
-    // document.getElementById('Celeste').style.display = 'none';
-    document.getElementById('federation').style.display = 'none';
-    document.getElementById('Republic').style.display = 'none';
-  } 
+    function leaveanimal() {
+    document.getElementById('celeste').style.display = 'none';
+    document.getElementById('terrestre').style.display = 'none';
+    document.getElementById('maritime').style.display = 'none';
+  }
   window.onload = leaveall();
-  window.onload = leavesw();
-  window.onload = getRepublic();
-  window.onload = getSeparatist();
+  window.onload = leaveanimal();
---
+++ js/indexscript.js
@@ -0,0 +1,104 @@
+
+  document.getElementById("drop0").addEventListener("mouseleave", mouseLeave0);
+  document.getElementById("drop1").addEventListener("mouseleave", mouseLeave1);
+  document.getElementById("drop2").addEventListener("onclick", mouseEnter2);
+  document.getElementById("drop2").addEventListener("mouseleave", mouseLeave2);
+
+  document.getElementById("drop3").addEventListener("onclick", mouseEnter3);  
+  document.getElementById("drop3").addEventListener("mouseleave", mouseLeave3);
+  
+  let b_m = document.getElementById("acc");
+  let dr = document.getElementById("drop0_1");
+  b_m.addEventListener("click", () =>{
+    if(getComputedStyle(dr).display != "none"){
+      mouseLeave0();
+    }
+    else
+    {
+      leaveall();
+  
+      document.getElementById("drop0_1").style.display = "block";   
+      document.getElementById("drop0_1").style.borderRadius = "10px";
+      document.getElementById("acc").innerHTML = "Accueil <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
+    }
+  })
+
+
+  function mouseLeave0()
+  {
+    leaveall();
+    document.getElementById("acc").innerHTML = "Accueil <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+  }
+  let b_msw = document.getElementById("sw");
+  let drmsw = document.getElementById("drop1_1");
+  b_msw.addEventListener("click", () => {
+    if (getComputedStyle(drmsw).display != "none") {
+      mouseLeave1();
+    }
+    else {
+
+      leaveall();
+      document.getElementById("drop1_1").style.display = "block";
+      document.getElementById("drop1_1").style.borderRadius = "10px";
+      document.getElementById("sw").innerHTML = "Star Wars <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
+    }
+  })
+  //Menu drop 2
+  function mouseEnter1() {
+    leaveall();
+    document.getElementById("sw").innerHTML = "Star Wars <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
+    document.getElementById("drop1_1").style.display = "block";
+    document.getElementById("drop1_1").style.borderRadius = "10px";
+  }
+  function mouseLeave1() {
+    leaveall();
+
+    document.getElementById("sw").innerHTML = "Star Wars <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+  }
+
+  function mouseEnter2()
+  {
+    leaveall();
+    document.getElementById("gamertime").innerHTML = "A vous de jouer <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
+  }
+  function mouseLeave2()
+  {
+    leaveall();
+    document.getElementById("gamertime").innerHTML = "A vous de jouer ↓";
+    // document.getElementById("Footer").style.transitionDuration = "0.3s";
+    // document.getElementById("Footer").style.transform = "translate(0px, 0px)";
+  }
+  function mouseEnter3()
+  {
+    leaveall();
+    // document.getElementById("Footer").style.transitionDuration = "0.3s";
+    // document.getElementById("Footer").style.transform = "translate(0px, 65px)";
+    document.getElementById("whoru").innerHTML = "Qui sommes-nous <img width='20px' src='icons/round_keyboard_arrow_left_black_18dp.png'>";
+  }
+  function mouseLeave3()
+  {
+    leaveall();
+    document.getElementById("whoru").innerHTML = "Qui sommes-nous ↓";
+  }
+  
+
+  //LEAVE ALL
+  function leaveall() {
+    document.getElementById("drop0_1").style.display = "none";
+    document.getElementById("drop1_1").style.display = "none";
+    document.getElementById("acc").innerHTML = "Accueil <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+    document.getElementById("sw").innerHTML = "Star Wars <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+    document.getElementById("mamif").innerHTML = "Animaux <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+    document.getElementById("gamertime").innerHTML = "A vous de jouer <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+    document.getElementById("whoru").innerHTML = "Qui sommes-nous <img width='20px' src='icons/round_keyboard_arrow_down_black_18dp.png'>";
+    document.getElementById("Footer").style.bottom = "27px";
+    document.getElementById("allrights").style.bottom = "0";
+  }
+  function replace_footer()
+  {
+    document.getElementById("Footer").style.bottom = "27px";
+    document.getElementById("allrights").style.bottom = "0";
+    document.getElementById("Footer").style.transition = "all 1s ease-out;";
+    document.getElementById("allrights").style.transition = "all 1s ease-out;";
+  }
+  window.onload = leaveall();
